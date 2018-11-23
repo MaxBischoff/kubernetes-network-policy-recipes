bb51d4fe0a8dfeb80956df965441df1352c2b4bb
Maximilian Bischoff <maximilianbischoff2@gmail.com>
2018-11-23 10:46:19 +0100

    CHanged image from redis to nginx, as the test framework currently cannot determine connections to redis

--- res/10-allowing-traffic-with-multiple-selectors/setup.sh
+++ res/10-allowing-traffic-with-multiple-selectors/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl run db --image=redis:4 --port 6379 --expose \
+kubectl run db --image=nginx --port 6379 --expose \
         --labels app=bookstore,role=db
 
 kubectl apply -f redis-allow-services.yaml
